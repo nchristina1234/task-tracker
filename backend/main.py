e05c9e1a40b48fbca81baf603d482f3ccf88d826
cnguye61 <cnguye61@asu.edu>
2026-05-31 22:29:56 -0700

    Add GET tasks endpoint

--- backend/main.py
+++ backend/main.py
@@ -1,7 +1,30 @@
 from fastapi import FastAPI
 
+#task data
+tasks = [
+    {
+        "id": 1,
+        "title": "Learn FastAPI",
+        "completed": False
+    },
+    {
+        "id": 2,
+        "title": "Create endpoints",
+        "completed": False
+    }
+]
+
+
 app = FastAPI()
 
+
+#home page endpoint
 @app.get("/")
 def root():
     return {"message": "Task tracker backend running"}
+
+
+#get tasks endpoint
+@app.get("/tasks")
+def get_tasks():
+    return tasks
