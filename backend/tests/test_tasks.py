99a8a03839cf703f946c79bf8e774309ebb00f82
Christina Nguyen <nchristina1234@gmail.com>
2026-07-13 05:50:36 -0700

    Add CRUD API tests

--- backend/tests/test_tasks.py
+++ backend/tests/test_tasks.py
@@ -32,3 +32,48 @@ def test_get_one_task():
     assert response.json()["id"] == task_id
     assert response.json()["title"] == task_data["title"]
     assert response.json()["completed"] == task_data["completed"]
+
+def test_update_task():
+    # Create a task to ensure there is one to update
+    task_data = {"title": "Test Task", "completed": False}
+    create_response = client.post("/tasks", json=task_data)
+    task_id = create_response.json()["id"]
+
+    # Update the created task
+    new_data = {"title": "Test Task", "completed": True}
+    response = client.patch(f"/tasks/{task_id}", json=new_data)
+    assert response.status_code == 200
+    assert response.json()["id"] == task_id
+    assert response.json()["title"] == new_data["title"]
+    assert response.json()["completed"] == new_data["completed"]
+
+def test_delete_task():
+    # Create a task to ensure there is one to delete
+    task_data = {"title": "Test Task", "completed": False}
+    create_response = client.post("/tasks", json=task_data)
+    task_id = create_response.json()["id"]
+
+    # Delete the created task
+    delete_response = client.delete(f"/tasks/{task_id}")
+    assert delete_response.status_code == 204
+
+    # Attempt to retrieve deleted task
+    response = client.get(f"/tasks/{task_id}")
+    assert response.status_code == 404
+    assert response.json() == {"detail": f"Task with ID {task_id} not found"}
+
+def test_update_nonexistent_task():
+    # Create a task to ensure there is one to delete
+    task_data = {"title": "Test Task", "completed": False}
+    create_response = client.post("/tasks", json=task_data)
+    task_id = create_response.json()["id"]
+
+    # Delete the created task
+    delete_response = client.delete(f"/tasks/{task_id}")
+    assert delete_response.status_code == 204
+
+    # Attempt to update the nonexistent_task
+    new_data = {"title": "Test Task", "completed": True}
+    response = client.patch(f"/tasks/{task_id}", json=new_data)
+    assert response.status_code == 404
+    assert response.json() == {"detail": f"Task with ID {task_id} not found"}
